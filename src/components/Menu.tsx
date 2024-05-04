7a3f8b1082e3d645242c7dd51fc2482e89720d6e
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-05-04 00:56:26 -0500

    Nabar complete. Start footer next.

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import CartIcon from "./CartIcon";
 
 const links = [
   { id: 1, title: "Homepage", url: "/" },
@@ -10,6 +11,8 @@ const links = [
   { id: 3, title: "Working Hours", url: "/" },
   { id: 4, title: "Contact", url: "/" },
 ];
+// Temp user
+const user = false;
 
 const Menu = () => {
   const [open, setOpen] = useState(false);
@@ -32,13 +35,27 @@ const Menu = () => {
           onClick={() => setOpen(false)}
         />
       )}
-      <nav>
-        {links.map((item) => (
-          <Link href={item.url} key={item.id}>
-            {item.title}
+      {open && (
+        <nav className="bg-red-500 text-white absolute right-0 top-24 h-[calc(100vh-6rem)] flex flex-col items-center justify-center text-3xl gap-8 w-full  z-10">
+          {links.map((item) => (
+            <Link href={item.url} key={item.id} onClick={() => setOpen(false)}>
+              {item.title}
+            </Link>
+          ))}
+          {!user ? (
+            <Link href="/login" onClick={() => setOpen(false)}>
+              Login
+            </Link>
+          ) : (
+            <Link href="/orders" onClick={() => setOpen(false)}>
+              Orders
+            </Link>
+          )}
+          <Link href="/cart" onClick={() => setOpen(false)}>
+            <CartIcon />
           </Link>
-        ))}
-      </nav>
+        </nav>
+      )}
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,40 @@
 import React from "react";
 import Menu from "./Menu";
 import Link from "next/link";
+import CartIcon from "./CartIcon";
+import Image from "next/image";
 
 const Navbar = () => {
+  // Temp user.
+  const user = false;
   return (
-    <nav className="flex justify-between items-center p-4 h-12 text-red-500 border-b-2 border-b-red-500 uppercase">
+    <nav className="md:h-24 flex justify-between items-center p-4 h-12 text-red-500 border-b-2 border-b-red-500 uppercase lg:px-20 xl:px-40 ">
+      {/* Left Links */}
+      <div className="hidden md:flex gap-8 flex-1">
+        <Link href={"/"}>Homepage</Link>
+        <Link href={"/menu"}>Menu</Link>
+        <Link href={"/contact"}>Contact</Link>
+      </div>
       {/* Logo */}
-      <div className="text-xl font-semibold">
+      <div className="text-xl md:font-semibold flex-1 md:text-center">
         <Link href={"/"}>GetFoody</Link>
       </div>
       {/* Mobile Menu */}
-      <div>
+      <div className="md:hidden">
         <Menu />
+      </div>
+      {/* Right Links */}
+      <div className="hidden md:flex gap-8 flex-1 justify-end">
+        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+          <Image src="/phone.png " alt="phone" width={20} height={20} />
+          <span>123-456-7890</span>
+        </div>
+        {!user ? (
+          <Link href={"/login"}>Login</Link>
+        ) : (
+          <Link href={"/orders"}>Orders</Link>
+        )}
+        <CartIcon />
       </div>
     </nav>
   );
