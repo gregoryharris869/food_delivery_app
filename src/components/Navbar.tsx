9c4139e0131d8d462fa8f2c9447af5be1580a04f
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-05-05 16:24:49 -0500

    Featured.tsx finished

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,38 +5,37 @@ import CartIcon from "./CartIcon";
 import Image from "next/image";
 
 const Navbar = () => {
-  // Temp user.
   const user = false;
   return (
-    <nav className="md:h-24 flex justify-between items-center p-4 h-12 text-red-500 border-b-2 border-b-red-500 uppercase lg:px-20 xl:px-40 ">
-      {/* Left Links */}
-      <div className="hidden md:flex gap-8 flex-1">
-        <Link href={"/"}>Homepage</Link>
-        <Link href={"/menu"}>Menu</Link>
-        <Link href={"/contact"}>Contact</Link>
+    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
+      {/* LEFT LINKS */}
+      <div className="hidden md:flex gap-4 flex-1">
+        <Link href="/">Homepage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Contact</Link>
       </div>
-      {/* Logo */}
-      <div className="text-xl md:font-semibold flex-1 md:text-center">
-        <Link href={"/"}>GetFoody</Link>
+      {/* LOGO */}
+      <div className="text-xl md:font-bold flex-1 md:text-center">
+        <Link href="/">Massimo</Link>
       </div>
-      {/* Mobile Menu */}
+      {/* MOBILE MENU */}
       <div className="md:hidden">
         <Menu />
       </div>
-      {/* Right Links */}
-      <div className="hidden md:flex gap-8 flex-1 justify-end">
+      {/* RIGHT LINKS */}
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
         <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
-          <Image src="/phone.png " alt="phone" width={20} height={20} />
-          <span>123-456-7890</span>
+          <Image src="/phone.png" alt="" width={20} height={20} />
+          <span>123 456 78</span>
         </div>
         {!user ? (
-          <Link href={"/login"}>Login</Link>
+          <Link href="/login">Login</Link>
         ) : (
-          <Link href={"/orders"}>Orders</Link>
+          <Link href="/orders">Orders</Link>
         )}
         <CartIcon />
       </div>
-    </nav>
+    </div>
   );
 };
 
